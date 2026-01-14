8ae25df4744a99c7cfc41c2c0f67f9b42e2bc219
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-01-14 20:21:43 +0500

    Edit

--- Practice-JavaScript/app.js
+++ Practice-JavaScript/app.js
@@ -6,11 +6,25 @@ let alltodo = []
 function rendertodo(){
     div.innerHTML = "";
     for(let i = 0; i < alltodo.length; i++){
-    div.innerHTML += `<h1>${alltodo[i]}</h1>`
+    div.innerHTML += `<h1>${alltodo[i]}</h1>
+    <button onclick="edittodo(${i})">Edit</button>
+    <button onclick="deletetodo(${i})">Delete</button>`
 }}
+
 
 function addtodo(){
     alltodo.push(input.value)
     rendertodo()
     input.value = ""
+   
+   
+}
+function deletetodo(index){
+    alltodo.splice(index , 1)
+    rendertodo()
+}
+function edittodo(index){
+    let newvalue = prompt("Enter The New Value")
+    alltodo[index] = newvalue
+    rendertodo()
 }
